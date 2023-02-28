ed14d5e3c6a51a1a48c97ff02451ab054ee96732
Nish1007 <114507560+Nish1007@users.noreply.github.com>
2023-02-28 14:42:58 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -57,9 +57,8 @@ class App extends Component<{}, IState> {
   
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
-  }
+   
+ 
 
   /**
    * Render the App react component
